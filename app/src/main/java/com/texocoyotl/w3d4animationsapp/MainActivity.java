f4546fc13c12443566525d4175d0a6da81937f62
Benjamin Molina <gaeldron.might@gmail.com>
2016-04-15 00:50:12 -0400

    The image for the puzzle now comes from a photo.

--- app/src/main/java/com/texocoyotl/w3d4animationsapp/MainActivity.java
+++ app/src/main/java/com/texocoyotl/w3d4animationsapp/MainActivity.java
@@ -2,12 +2,15 @@ package com.texocoyotl.w3d4animationsapp;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.app.MediaRouteButton;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -17,9 +20,14 @@ public class MainActivity extends AppCompatActivity {
     private static final int PUZZLE_WIDTH = 3;
     private static final int CELL_PADDING = 10;
     private static final String PUZZLE_KEY = "puzzle";
+    private static final String BITMAP_KEY = "bitmap";
+    private static final int PICK_IMAGE = 1;
+
     private int emptyPosition;
     private Puzzle puzzle;
     private ImageView[] imageGrid;
+    private Bitmap mPuzzleBitmap;
+    private Button mLoadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Point size = new Point();
         //getWindowManager().getDefaultDisplay().getSize(size);
-
-        if (savedInstanceState != null){
-            puzzle = (Puzzle) savedInstanceState.getParcelable(PUZZLE_KEY);
-        }
-        else {
-            puzzle = new Puzzle(PUZZLE_WIDTH);
-        }
-
-        Log.d(TAG, "onCreate: size " + puzzle);
-
-        Bitmap bp = BitmapFactory.decodeResource(getResources(), R.drawable.grumpy);
-        final Bitmap cropped = Utils.cropBitmap(bp);
-
+        mLoadButton = (Button) findViewById(R.id.loadButton);
         imageGrid= new ImageView[9];
 
         imageGrid[0] = (ImageView) findViewById(R.id.img0);
@@ -52,6 +48,26 @@ public class MainActivity extends AppCompatActivity {
         imageGrid[6] = (ImageView) findViewById(R.id.img6);
         imageGrid[7] = (ImageView) findViewById(R.id.img7);
         imageGrid[8] = (ImageView) findViewById(R.id.img8);
+
+        if (savedInstanceState != null){
+            puzzle = (Puzzle) savedInstanceState.getParcelable(PUZZLE_KEY);
+            mPuzzleBitmap = (Bitmap) savedInstanceState.getParcelable(BITMAP_KEY);
+            if (mPuzzleBitmap != null) {
+                mLoadButton.setVisibility(View.GONE);
+                loadPuzzle(mPuzzleBitmap);
+            }
+        }
+        else {
+            puzzle = new Puzzle(PUZZLE_WIDTH);
+        }
+
+        Log.d(TAG, "onCreate: size " + puzzle);
+
+
+    }
+
+    private void loadPuzzle(Bitmap bp){
+        final Bitmap cropped = Utils.cropBitmap(bp);
 
         for (int i = 0; i < PUZZLE_WIDTH * PUZZLE_WIDTH; i++) {
 
@@ -98,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
                                 super.onAnimationEnd(animation);
 
                                 if (puzzle.isSolved()){
-                                    Toast.makeText(MainActivity.this, "Finished!", Toast.LENGTH_LONG).show();
+                                    Toast.makeText(MainActivity.this, "Victory!!!", Toast.LENGTH_LONG).show();
 
                                     int x = puzzle.get(emptyPosition) % PUZZLE_WIDTH;
                                     int y = puzzle.get(emptyPosition) / PUZZLE_WIDTH;
@@ -118,19 +134,32 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         }
-
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putParcelable(PUZZLE_KEY, puzzle);
+        outState.putParcelable(BITMAP_KEY, mPuzzleBitmap);
+
         super.onSaveInstanceState(outState);
     }
 
-    public void move(View view) {
-//        Button b = (Button) findViewById(R.id.button);
-//        ObjectAnimator fadeAltAnim = ObjectAnimator.ofFloat(b, View.TRANSLATION_X, 10);
-//        fadeAltAnim.start();
-        view.animate().setDuration(100).translationXBy(150).translationYBy(150);
+    public void getPicture(View view) {
+
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data != null) {
+            mPuzzleBitmap = (Bitmap) data.getExtras().get("data");
+            //mPuzzleBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.grumpy);
+            mLoadButton.setVisibility(View.GONE);
+            loadPuzzle(mPuzzleBitmap);
+        }
+    }
+
 }
